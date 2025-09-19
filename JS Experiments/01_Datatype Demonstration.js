865597f8993cc8d2a136446b22b97bbe7b23b186
pb4949 <parnilrbalawade@gmail.com>
2025-09-19 17:06:49 +0530

    Add files via upload

---
+++ JS Experiments/01_Datatype Demonstration.js
@@ -0,0 +1,11 @@
+let theName = "xyz"
+let age =  17
+let isStudent = true
+
+console.log(theName)
+console.log(age)
+console.log(isStudent)
+console.log(typeof(theName))
+console.log(typeof(age))
+console.log(typeof(isStudent))
+
