4d4c3182f742e17bc6d47903f3be13340a7e6e61
pb4949 <parnilrbalawade@gmail.com>
2025-09-19 17:35:55 +0530

    Create 04_Age Checker.js

---
+++ 06_JS Experiments/04_Age Checker.js
@@ -0,0 +1,9 @@
+let user
+let age
+
+user = prompt("Full Legal Government Issued Name: ")
+age = Number(prompt("Enter Age: "))
+if (age >= 18)
+    console.log("Eligible to vote")
+else
+    console.log("Ineligible to vote")
