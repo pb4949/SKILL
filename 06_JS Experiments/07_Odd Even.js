edf1a473a01d63a4d4469a39ea4e51387d721b7c
pb4949 <parnilrbalawade@gmail.com>
2025-09-19 17:49:33 +0530

    Create 07_Odd Even.js

---
+++ 06_JS Experiments/07_Odd Even.js
@@ -0,0 +1,9 @@
+let n
+n = Number(prompt("Enter Number: "))
+
+if (n % 1 != 0)
+    console.log("Number is a decimal, doesn't apply")
+else if (n % 2 == 0)
+    console.log("Number is even")
+else
+    console.log("Number is odd")
