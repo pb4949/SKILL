2c68df3a46700d5397c3b13b6d32acb8bae46446
pb4949 <parnilrbalawade@gmail.com>
2025-09-19 17:40:19 +0530

    Create 05_Mark Grades.js

---
+++ 06_JS Experiments/05_Mark Grades.js
@@ -0,0 +1,20 @@
+let sub1
+let sub2
+let sub3
+
+sub1 = Number(prompt("Enter Percentage of Subject 1: "))
+sub2 = Number(prompt("Enter Percentage of Subject 2: "))
+sub3 = Number(prompt("Enter Percentage of Subject 3: "))
+
+let perc = (sub1 + sub2 + sub3) / 3
+console.log("Total Percentage: ", perc)
+console.log("")
+
+if (perc >= 90)
+    console.log("Grade A")
+else if (perc >= 75)
+    console.log("Grade B")
+else if (perc >= 50)
+    console.log("Grade C")
+else
+    console.log("Grade D")
