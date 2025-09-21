ce4150f4a96dbdc435a5194f9519f787a3ce58f6
pb4949 <parnilrbalawade@gmail.com>
2025-09-21 15:43:47 +0530

    Update 05_Mark Grades.js

--- 06_JS Experiments/05_Mark Grades.js
+++ 06_JS Experiments/05_Mark Grades.js
@@ -1,10 +1,6 @@
-let sub1
-let sub2
-let sub3
-
-sub1 = Number(prompt("Enter Percentage of Subject 1: "))
-sub2 = Number(prompt("Enter Percentage of Subject 2: "))
-sub3 = Number(prompt("Enter Percentage of Subject 3: "))
+let sub1 = Number(prompt("Enter Percentage of Subject 1: "))
+let sub2 = Number(prompt("Enter Percentage of Subject 2: "))
+let sub3 = Number(prompt("Enter Percentage of Subject 3: "))
 
 let perc = (sub1 + sub2 + sub3) / 3
 console.log("Total Percentage: ", perc)
