e31706974fe5fad20e3a9b614466340cf43da833
pb4949 <parnilrbalawade@gmail.com>
2025-09-21 16:09:32 +0530

    Update 08_Greatest Number.js

--- 06_JS Experiments/08_Greatest Number.js
+++ 06_JS Experiments/08_Greatest Number.js
@@ -1,10 +1,6 @@
-let n1
-let n2
-let n3
-
-n1 = Number(prompt("Enter first number: "))
-n2 = Number(prompt("Enter second number: "))
-n3 = Number(prompt("Enter third number: "))
+let n1 = Number(prompt("Enter first number: "))
+let n2 = Number(prompt("Enter second number: "))
+let n3 = Number(prompt("Enter third number: "))
 
 let ary = [n1, n2, n3]
 let i = 0
