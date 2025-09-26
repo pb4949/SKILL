f415b69d02acdc2a409a2886fda8fb053cad74b4
pb4949 <parnilrbalawade@gmail.com>
2025-09-26 18:15:48 +0530

    Create 02_MinMax Numbers.js

---
+++ 09_Arrays/02_MinMax Numbers.js
@@ -0,0 +1,12 @@
+let ary = [1, 2, 534, 4, 5, 6, -45, 8, 9, 10]
+let i = 0
+let max = 0
+let min = 0
+for (i = 0; i < 10; i++)
+{
+    if (ary[i] > ary[max])
+        max = i;
+    else if (ary[i] < ary[min])
+        min = i;
+    console.log(ary[i])
+}
