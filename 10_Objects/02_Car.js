55418210c00104ab2e70c598efd875ebb027bd39
pb4949 <parnilrbalawade@gmail.com>
2025-09-26 18:25:02 +0530

    Create 02_Car.js

---
+++ 10_Objects/02_Car.js
@@ -0,0 +1,8 @@
+const car = {brand:"Ford", model:"F-150"}
+console.log(car.brand)
+console.log(car.model)
+car.year = 2022
+console.log(car.year)
+car.model = "F-250"
+console.log(car.model)
+delete car.year
