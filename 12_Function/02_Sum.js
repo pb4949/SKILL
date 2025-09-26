921b5ef3d72d7926eced3654a8eab46a1075a02c
pb4949 <parnilrbalawade@gmail.com>
2025-09-26 19:31:22 +0530

    Update 02_Sum.js

--- 12_Function/02_Sum.js
+++ 12_Function/02_Sum.js
@@ -3,7 +3,7 @@ function add(a, b)
     return a + b;
 }   
 
-a = Number(prompt("Enter a number: "));
-b = Number(prompt("Enter another number: "));
+let a = Number(prompt("Enter a number: "));
+let b = Number(prompt("Enter another number: "));
 
 console.log("Sum is " + add(a, b));
